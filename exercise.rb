a2d07d1bceffa645879dc2d0c8332c62badb1c8e
Sushma <sushma.m812@gmail.com>
2017-09-25 16:09:16 -0700

    first commit

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,29 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    #so if str is "hi my name is soandso and Someperson"
+    #we want to output to "hi my name is marklar and Marklar"
+    getarray = str.split(" ") #will convert string into array of words
+      getarray.each do |i| #loop
+        index = getarray.index(i) 
+        if i.length > 4 #if the word exceeds 4 characters
+          if /[[:upper:]]/.match(i[0]) #if the word has capitalized first letter
+            if /[[:punct:]]/.match(i[i.length-1]) #if the word has punctuation in the end
+              getarray[index] = "Marklar"+i[i.length-1]
+            else
+              getarray[index] = "Marklar"
+            end
+          else
+            if /[[:punct:]]/.match(i[i.length-1]) #if the word has punctuation in the end
+              getarray[index] = "marklar"+i[i.length-1]
+            else
+              getarray[index] = "marklar"
+            end
+          end
+        end
+      end
+      
+      return getarray.join(" ") #will convert  array of words into string
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +36,22 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
-  end
+    add = 1 #set first number of Fibonacci sequence
+    newarr = [] #create a new array to give
+    newarr.push(add) #push 1 into that new array
+    count = 0 #initialize while loop
+    getsum = 0 #get the sum
+
+    while count < (nth-1)  do #looping until nth number
+      if newarr[count]%2 == 0 #checking if the number is even
+        getsum += newarr[count] #adding the sum
+      end
+      newarr.push(add) #we add the number into the looop
+      add = add+newarr[count] #add number for Fibonacci sequence
+      count +=1
+    end
+    
+    return getsum
+    end
 
 end
